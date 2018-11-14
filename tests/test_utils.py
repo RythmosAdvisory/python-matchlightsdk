6fa61ce7df1868a5c4fad52c320ffce615c0f9f5
shu zOMG chen <shu.chen@freelancedreams.com>
2018-11-14 00:46:14 -0800

    Use pytest parametrize instead

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,33 +1,33 @@
 """Unit tests the utils module of Matchlight SDK."""
+import pytest
+
 import matchlight
 
 
-def test_blind_name():
+@pytest.mark.parametrize('test_input,expected', [
+    (None, '*****'),
+    ('', '*****'),
+    ('a', 'a****'),
+    ('foobar', 'f****'),
+    ('terbiumlabs', 't****'),
+])
+def test_blind_name(test_input, expected):
     """Verify test cases for blind_name."""
-    test_cases = [
-        (None, '*****'),
-        ('', '*****'),
-        ('a', 'a****'),
-        ('foobar', 'f****'),
-        ('terbiumlabs', 't****'),
-    ]
-    for case, expected in test_cases:
-        assert matchlight.utils.blind_name(case) == expected
+    assert matchlight.utils.blind_name(test_input) == expected
 
 
-def test_blind_email():
+@pytest.mark.parametrize('test_input,expected', [
+    (None, '*****'),
+    ('', '*****'),
+    ('a@terbiumlabs.com', 'a****@terbiumlabs.com'),
+    ('ab@terbiumlabs.com', 'a****@terbiumlabs.com'),
+    ('abc@terbiumlabs.com', 'a****@terbiumlabs.com'),
+    ('abcd@terbiumlabs.com', 'ab****@terbiumlabs.com'),
+    ('abcde@terbiumlabs.com', 'ab****@terbiumlabs.com'),
+    ('abcdef@terbiumlabs.com', 'abc****@terbiumlabs.com'),
+    ('abcdefg@terbiumlabs.com', 'abc****@terbiumlabs.com'),
+    ('abcdefgh@terbiumlabs.com', 'abc****@terbiumlabs.com'),
+])
+def test_blind_email(test_input, expected):
     """Verify test cases for blind_email."""
-    test_cases = [
-        (None, '*****'),
-        ('', '*****'),
-        ('a@terbiumlabs.com', 'a****@terbiumlabs.com'),
-        ('ab@terbiumlabs.com', 'a****@terbiumlabs.com'),
-        ('abc@terbiumlabs.com', 'a****@terbiumlabs.com'),
-        ('abcd@terbiumlabs.com', 'ab****@terbiumlabs.com'),
-        ('abcde@terbiumlabs.com', 'ab****@terbiumlabs.com'),
-        ('abcdef@terbiumlabs.com', 'abc****@terbiumlabs.com'),
-        ('abcdefg@terbiumlabs.com', 'abc****@terbiumlabs.com'),
-        ('abcdefgh@terbiumlabs.com', 'abc****@terbiumlabs.com'),
-    ]
-    for case, expected in test_cases:
-        assert matchlight.utils.blind_email(case) == expected
+    assert matchlight.utils.blind_email(test_input) == expected
