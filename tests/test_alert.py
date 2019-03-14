3d8be22a6ed12a282974cf4ee441bbfca65939f1
Alex C. Viana <alexcostaviana@gmail.com>
2019-03-14 12:06:53 -0500

    Replace HTTPretty with Responses (#75)
    * Added a working proof of concept test using responses in test_record.py. For #73
    
    * Fixed dupliacted test function name for POC.
    
    * Add a missing pytest fixture.
    
    * Fixed a line length flake8 error
    
    * Improved assert statements on responses prototype test.
    
    * Replaced all httpretty tests with responses in test_records.py.
    
    * Replaced all httpretty tests with responses in test_project.py.
    
    * Replaced all httpretty tests with responses in test_feed.py.
    
    * Removed one more thing in test_feed.py
    
    * Need feedback on failing tests in test_alerts.py.
    
    * Removed HTTPretty from requirements files.
    
    * Added some parameter labels for test_record.py
    
    * Fix ‘test_alert_edit’ by isolating different test cases
    
    * Pin ‘responses’ version
    
    * fix ‘test_alert_filter_record’ by converting timestamps properly.

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,48 +1,44 @@
 """Unit tests the alert methods of the Matchlight SDK."""
 import datetime
-import json
 import time
 import uuid
 
-import httpretty
-import pytest
+import responses
 
 import matchlight
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_dates(connection, alert, alert_payload):
     """Verifies alert date objects are converted correctly."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?limit=50'.format(
-            matchlight.MATCHLIGHT_API_URL_V2
-        ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?limit=50'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'alerts': [alert_payload]},
         status=200
     )
     alerts = connection.alerts.filter(limit=50)
     assert isinstance(alerts[0].date, datetime.datetime)
     assert isinstance(alerts[0].last_modified, datetime.datetime)
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter(connection, alert, alert_payload):
     """Verifies alert listing and filtering."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?limit=50'.format(
-            matchlight.MATCHLIGHT_API_URL_V2
-        ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?limit=50'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'alerts': [alert_payload]},
         status=200
     )
     alerts = connection.alerts.filter(limit=50)
     assert len(alerts) == 1
     assert alerts[0].id == alert.id
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter_seen(connection, alert, alert_payload):
     """Verifies alert filtering on 'seen'."""
     # Create opposite alert
@@ -51,35 +47,35 @@ def test_alert_filter_seen(connection, alert, alert_payload):
     unseen_payload['id'] = str(uuid.uuid4())
 
     # Get seen alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?seen=1&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?seen=1&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2
         ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload]},
         status=200
     )
-
     alerts = connection.alerts.filter(limit=50, seen=True)
     assert len(alerts) == 1
     assert alerts[0].id == alert_payload['id']
+    assert len(responses.calls) == 1
 
     # Get unseen alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?seen=0&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?seen=0&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2
         ),
-        body=json.dumps({'alerts': [unseen_payload]}),
-        content_type='application/json',
+        json={'alerts': [unseen_payload]},
         status=200
     )
-
     alerts = connection.alerts.filter(limit=50, seen=False)
     assert len(alerts) == 1
     assert alerts[0].id == unseen_payload['id']
+    assert len(responses.calls) == 2
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter_archived(connection, alert, alert_payload):
     """Verifies alert filtering on 'archived'."""
     # Create opposite alert
@@ -88,199 +84,215 @@ def test_alert_filter_archived(connection, alert, alert_payload):
     unarchived_payload['id'] = str(uuid.uuid4())
 
     # Get archived alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?archived=1&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?archived=1&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2
         ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload]},
         status=200
     )
-
     alerts = connection.alerts.filter(limit=50, archived=True)
     assert len(alerts) == 1
     assert alerts[0].id == alert_payload['id']
+    assert len(responses.calls) == 1
 
     # Get unarchived alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?archived=0&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?archived=0&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2
         ),
-        body=json.dumps({'alerts': [unarchived_payload]}),
-        content_type='application/json',
+        json={'alerts': [unarchived_payload]},
         status=200
     )
-
     alerts = connection.alerts.filter(limit=50, archived=False)
     assert len(alerts) == 1
     assert alerts[0].id == unarchived_payload['id']
+    assert len(responses.calls) == 2
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter_project(connection, alert, alert_payload, project):
     """Verifies alert filtering on 'upload_token'."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?upload_token_filter={}&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?upload_token_filter={}&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             project.upload_token
         ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload]},
         status=200
     )
 
     alerts = connection.alerts.filter(limit=50, project=project)
     assert len(alerts) == 1
     assert alerts[0].id == alert_payload['id']
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter_record(connection, alert, alert_payload,
                              document_record):
     """Verifies alert filtering on 'record_id'."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?record_id_filter={}&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?record_id_filter={}&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             document_record.id
         ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload]},
         status=200
     )
 
     alerts = connection.alerts.filter(limit=50, record=document_record)
     assert len(alerts) == 1
     assert alerts[0].id == alert_payload['id']
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_filter_mtime(connection, alert, alert_payload):
     """Verifies alert filtering on 'seen'."""
-    now = time.time()
+    now = int(time.time())
     # Two days ago
     old_payload = alert_payload.copy()
     old_payload['mtime'] = now - 172800
     old_payload['id'] = str(uuid.uuid4())
 
     # Get all alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?mtime={}&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?mtime={}&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             now - 259200
         ),
-        body=json.dumps({'alerts': [alert_payload, old_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload, old_payload]},
         status=200
     )
 
     alerts = connection.alerts.filter(
         limit=50,
-        last_modified=datetime.datetime.fromtimestamp(now - 259200)
+        last_modified=datetime.datetime.utcfromtimestamp(now - 259200)
     )
     assert len(alerts) == 2
+    assert len(responses.calls) == 1
 
     # Get new alerts
-    httpretty.register_uri(
-        httpretty.GET, '{}/alerts?mtime={}&limit=50'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alerts?mtime={}&limit=50'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             now - 86400
         ),
-        body=json.dumps({'alerts': [alert_payload]}),
-        content_type='application/json',
+        json={'alerts': [alert_payload]},
         status=200
     )
 
     alerts = connection.alerts.filter(
         limit=50,
-        last_modified=datetime.datetime.fromtimestamp(now - 86400)
+        last_modified=datetime.datetime.utcfromtimestamp(now - 86400)
     )
     assert len(alerts) == 1
     assert alerts[0].id == alert_payload['id']
+    assert len(responses.calls) == 2
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_edit(connection, alert, alert_payload):
     """Verifies alert editing."""
     # Do nothing
-    httpretty.register_uri(
-        httpretty.POST, '{}/alert/{}/edit'.format(
-            matchlight.MATCHLIGHT_API_URL_V2,
-            alert.id,
-        ),
-        body=json.dumps({
-            'archived': True,
-            'seen': True
-        }),
-        status=200
-    )
-    response = connection.alerts.edit(alert.id)
-    assert response['seen'] is True
-    assert response['archived'] is True
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            method=rsps.POST,
+            url='{}/alert/{}/edit'.format(
+                matchlight.MATCHLIGHT_API_URL_V2,
+                alert.id,
+            ),
+            json={
+                'archived': True,
+                'seen': True
+            },
+            status=200
+        )
+
+        response = connection.alerts.edit(alert.id)
+        assert response['seen'] is True
+        assert response['archived'] is True
 
     # Un-archive
-    httpretty.register_uri(
-        httpretty.POST, '{}/alert/{}/edit'.format(
-            matchlight.MATCHLIGHT_API_URL_V2,
-            alert.id,
-        ),
-        body=json.dumps({
-            'archived': False,
-            'seen': True
-        }),
-        status=200
-    )
-    response = connection.alerts.edit(alert.id, archived=False)
-    assert response['seen'] is True
-    assert response['archived'] is False
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            method=rsps.POST,
+            url='{}/alert/{}/edit'.format(
+                matchlight.MATCHLIGHT_API_URL_V2,
+                alert.id,
+            ),
+            json={
+                'archived': False,
+                'seen': True
+            },
+            status=200
+        )
+        response = connection.alerts.edit(alert.id, archived=False)
+
+        assert response['seen'] is True
+        assert response['archived'] is False
 
     # Un-see
-    httpretty.register_uri(
-        httpretty.POST, '{}/alert/{}/edit'.format(
-            matchlight.MATCHLIGHT_API_URL_V2,
-            alert.id,
-        ),
-        body=json.dumps({
-            'archived': False,
-            'seen': False
-        }),
-        status=200
-    )
-    response = connection.alerts.edit(alert.id, seen=False)
-    assert response['seen'] is False
-    assert response['archived'] is False
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            method=rsps.POST,
+            url='{}/alert/{}/edit'.format(
+                matchlight.MATCHLIGHT_API_URL_V2,
+                alert.id,
+            ),
+            json={
+                'archived': False,
+                'seen': False
+            },
+            status=200
+        )
+        response = connection.alerts.edit(alert.id, seen=False)
+        assert response['seen'] is False
+        assert response['archived'] is False
 
     # Both
-    httpretty.register_uri(
-        httpretty.POST, '{}/alert/{}/edit'.format(
-            matchlight.MATCHLIGHT_API_URL_V2,
-            alert.id,
-        ),
-        body=json.dumps({
-            'archived': True,
-            'seen': True
-        }),
-        status=200
-    )
-    response = connection.alerts.edit(alert.id, seen=True, archived=True)
-    assert response['seen'] is True
-    assert response['archived'] is True
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            method=rsps.POST,
+            url='{}/alert/{}/edit'.format(
+                matchlight.MATCHLIGHT_API_URL_V2,
+                alert.id,
+            ),
+            json={
+                'archived': True,
+                'seen': True
+            },
+            status=200
+        )
+        response = connection.alerts.edit(alert.id, seen=True, archived=True)
+        assert response['seen'] is True
+        assert response['archived'] is True
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_alert_details(connection, alert, alert_details_pii_payload):
     """Verifies alert get details responses."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/alert/{}/details'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/alert/{}/details'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             alert.id
         ),
-        body=json.dumps(alert_details_pii_payload),
-        content_type='application/json',
+        json=alert_details_pii_payload,
         status=200
     )
 
     details_ = connection.alerts.get_details(alert)
     assert details_ == alert_details_pii_payload
+    assert len(responses.calls) == 1
 
     details_ = connection.alerts.get_details(alert.id)
     assert details_ == alert_details_pii_payload
+    assert len(responses.calls) == 2
--- tests/test_project.py
+++ tests/test_project.py
@@ -2,14 +2,14 @@
 import datetime
 import json
 
-import httpretty
 import pytest
+import responses
 import six.moves
 
 import matchlight
 
 
-PROJECT_TYPES = ('bulk_pii', 'document', 'pii', 'source_code')
+PROJECT_TYPES = ('document', 'pii', 'source_code')
 
 
 def test_project_last_modified(project):
@@ -18,76 +18,91 @@ def test_project_last_modified(project):
         project.last_date_modified)
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_add(connection, project_name, project_payload,
                      project_type, upload_token):
     """Verifies project creation."""
-    httpretty.register_uri(
-        httpretty.POST, '{}/project/add'.format(
-            matchlight.MATCHLIGHT_API_URL_V2),
-        body=json.dumps({'data': project_payload}),
-        content_type='application/json', status=200)
+    responses.add(
+        method=responses.POST,
+        url='{}/project/add'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'data': project_payload},
+        status=200
+    )
     project = connection.projects.add(project_name, project_type)
     assert project.upload_token == upload_token
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_edit(connection, project, project_payload):
     """Verifies project renaming."""
-    httpretty.register_uri(
-        httpretty.POST, '{}/project/{}/edit'.format(
+    responses.add(
+        method=responses.POST,
+        url='{}/project/{}/edit'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             project.upload_token,
         ),
-        body='{}', status=200)
+        json='{}',
+        status=200
+    )
     new_name = 'Test Project 1'
     connection.projects.edit(project, new_name)
     assert project.name == new_name
+    assert len(responses.calls) == 1
 
     new_name = 'Test Project 2'
-    httpretty.register_uri(
-        httpretty.GET, '{}/project/{}'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/project/{}'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             project.upload_token,
         ),
-        body=json.dumps(project_payload),
-        content_type='application/json', status=200)
-    project = connection.projects.edit(project.upload_token, new_name)
+        json=project_payload,
+        status=200
+    )
+    project = connection.projects.edit(project, new_name)
     assert project.name == new_name
+    assert len(responses.calls) == 2
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_delete(connection, project, project_payload):
     """Verifies project deletion."""
-    httpretty.register_uri(
-        httpretty.POST, '{}/project/{}/delete'.format(
+    responses.add(
+        method=responses.POST,
+        url='{}/project/{}/delete'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
             project.upload_token,
         ),
-        body='{}', status=200)
-    httpretty.register_uri(
-        httpretty.GET, '{}/project/{}'.format(
-            matchlight.MATCHLIGHT_API_URL_V2,
-            project.upload_token,
-        ),
-        body=json.dumps(project_payload),
-        content_type='application/json', status=200)
+        json='{}',
+        status=200
+    )
+    # responses.add(
+    #     method=responses.GET,
+    #     url='{}/project/{}'.format(
+    #         matchlight.MATCHLIGHT_API_URL_V2,
+    #         project.upload_token,
+    #     ),
+    #     json=project_payload,
+    #     status=200
+    # )
     connection.projects.delete(project)
+    assert len(responses.calls) == 1
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_filter(connection, project_payload, project):
     """Verifies project listing and filtering by type."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/projects'.format(
-            matchlight.MATCHLIGHT_API_URL_V2),
-        body=json.dumps({'data': [project_payload]}),
-        content_type='application/json', status=200)
+    responses.add(
+        method=responses.GET,
+        url='{}/projects'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'data': [project_payload]},
+        status=200
+    )
     projects = connection.projects.filter()
     assert len(projects) == 1
     assert projects[0].upload_token == project.upload_token
-
-    httpretty.reset()
+    assert len(responses.calls) == 1
 
     project_list = [project_payload]
     for _ in six.moves.range(5):
@@ -98,52 +113,73 @@ def test_project_filter(connection, project_payload, project):
             payload['project_type'] = project_type
             break
         project_list.append(payload)
-    httpretty.register_uri(
-        httpretty.GET, '{}/projects'.format(
-            matchlight.MATCHLIGHT_API_URL_V2),
-        body=json.dumps({'data': project_list}),
-        content_type='application/json', status=200)
+    responses.add(
+        method=responses.GET,
+        url='{}/projects'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'data': project_list},
+        status=200
+    )
     projects = connection.projects.filter(project_type=project.project_type)
     assert len(projects) == 1
     assert projects[0].project_type == project.project_type
+    assert len(responses.calls) == 2
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_get(connection, project_payload, project):
     """Verifies project retrieval."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/project/{}'.format(
+    responses.add(
+        method=responses.GET,
+        url='{}/project/{}'.format(
             matchlight.MATCHLIGHT_API_URL_V2,
-            project.upload_token),
-        responses=[
-            httpretty.Response(body=json.dumps(project_payload),
-                               content_type='application/json',
-                               status=200),
-            httpretty.Response(body='{}', content_type='application/json',
-                               status=404),
-            httpretty.Response(body='{}', content_type='application/json',
-                               status=500),
-        ])
+            project.upload_token
+        ),
+        body=json.dumps(project_payload),
+        status=200
+    )
+    responses.add(
+        method=responses.GET,
+        url='{}/project/{}'.format(
+            matchlight.MATCHLIGHT_API_URL_V2,
+            project.upload_token
+        ),
+        body='{}',
+        status=404
+    )
+    responses.add(
+        method=responses.GET,
+        url='{}/project/{}'.format(
+            matchlight.MATCHLIGHT_API_URL_V2,
+            project.upload_token
+        ),
+        body='{}',
+        status=500
+    )
 
     project_ = connection.projects.get(project.upload_token)
     assert project.upload_token == project_.upload_token
+    assert len(responses.calls) == 1
 
     project_ = connection.projects.get(project.upload_token)
     assert project_ is None
+    assert len(responses.calls) == 2
 
-    with pytest.raises(matchlight.error.ConnectionError):
+    with pytest.raises(matchlight.error.APIError):
         connection.projects.get(project.upload_token)
+    assert len(responses.calls) == 3
 
 
-@pytest.mark.httpretty
+@responses.activate
 def test_project_iteration(connection, project, project_payload):
     """Verifies project iteration."""
-    httpretty.register_uri(
-        httpretty.GET, '{}/projects'.format(matchlight.MATCHLIGHT_API_URL_V2),
-        body=json.dumps({'data': [project_payload]}),
-        content_type='application/json', status=200,
+    responses.add(
+        method=responses.GET,
+        url='{}/projects'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'data': [project_payload]},
+        status=200,
     )
     projects_iterable = iter(connection.projects)
     assert next(projects_iterable).details == project.details
     with pytest.raises(StopIteration):
         next(projects_iterable)
+    assert len(responses.calls) == 1
