3cbcf8efb91f46610620e0ab934d2e629a01cdb5
Matt de Young <matt@terbiumlabs.com>
2019-05-02 14:23:13 -0400

    Docuemnt Record length limits & raise error if too long.

--- tests/test_record.py
+++ tests/test_record.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 import io
 import json
+import random
+import string
 import time
 import uuid
 
@@ -175,6 +177,22 @@ def test_record_add_document_prototype(
         offline=True)
     assert isinstance(record, dict)
     assert len(responses.calls) == 1
+
+    # Test that a nice error is returned for too long input
+    with pytest.raises(matchlight.error.SDKError) as err_info:
+        connection.records.add_document(
+            project=project,
+            name=document['name'],
+            description=document['description'],
+            content=''.join(
+                random.choices(string.ascii_uppercase + string.digits, k=841)
+            ),
+        )
+
+    assert str(err_info.value) == (
+        'Fingerprinter Failed: the maximum length of a Document record'
+        ' is 840 characters.'
+    )
 
 
 @responses.activate
