3fb6934def0248044ef042ec039d3f6773c8af6d
Matt de Young <Matt@TerbiumLabs.com>
2019-05-21 19:56:36 -0400

    Fix regular Search with the 'email' parameter (#82)
    * Handle multidimensional lists returned from LibFP
    
    * Fix for PII search date formats.
    
    * Make search tests timezone independent.

--- tests/test_search.py
+++ tests/test_search.py
@@ -22,22 +22,28 @@ def test_pii_search(connection, pii_search_email_only_results):
     ) == [
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2018, 7, 25, 20, 0, 44),
+            'ts': datetime.datetime.strptime(
+                '2018-07-25T20:00:44',
+                '%Y-%m-%dT%H:%M:%S'
+            ),
             'source': 'Exactis Breach June 2018'
         },
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2017, 1, 25, 2, 35, 4),
+            'ts': datetime.datetime.strptime(
+                '2017-01-25T02:35:04',
+                '%Y-%m-%dT%H:%M:%S'
+            ),
             'source': 'https://pastebin.com/raw.php?i=1DgbtSZc'
         },
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2017, 2, 8, 7, 59, 39),
+            'ts': datetime.datetime.fromtimestamp(1556221970.0),
             'source': 'Zoosk Breach Nov 2016'
         },
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2016, 11, 24, 0, 18, 27),
+            'ts': datetime.datetime.fromtimestamp(1558333205.0),
             'source': 'https://www.reddit.com/r/AskReddit/comments/3oqj4a'
         }
     ]
@@ -63,12 +69,18 @@ def test_pii_search_limit(connection, pii_search_email_only_results):
     ) == [
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2018, 7, 25, 20, 0, 44),
+            'ts': datetime.datetime.strptime(
+                '2018-07-25T20:00:44',
+                '%Y-%m-%dT%H:%M:%S'
+            ),
             'source': 'Exactis Breach June 2018'
         },
         {
             'fields': ['email'],
-            'ts': datetime.datetime(2017, 1, 25, 2, 35, 4),
+            'ts': datetime.datetime.strptime(
+                '2017-01-25T02:35:04',
+                '%Y-%m-%dT%H:%M:%S'
+            ),
             'source': 'https://pastebin.com/raw.php?i=1DgbtSZc'
         }
     ]
@@ -77,3 +89,34 @@ def test_pii_search_limit(connection, pii_search_email_only_results):
         'email_fingerprints': [['ff71225ace46c2b0'], ['293de73a18a5e063']],
         'limit': 2,
     }
+
+
+@responses.activate
+def test_search_email(connection, search_email_only_results):
+    """Verifies generic search when using email."""
+    responses.add(
+        method=responses.POST,
+        url='{}/detailed_search'.format(matchlight.MATCHLIGHT_API_URL_V2),
+        json={'results': search_email_only_results},
+        status=200
+    )
+
+    assert list(
+        connection.search(email='familybird@terbiumlabs.com')
+    ) == [
+        {
+            'score': 800,
+            'ts': datetime.datetime.fromtimestamp(1453298293.0),
+            'url': (
+                'http://blockchainbdgpzk.onion/tx/4f4097992b89156'
+                '690817556fc3f540535bdfadde06661c9cae21d500943f970'
+            )
+        }, {
+            'score': 800,
+            'ts': datetime.datetime.fromtimestamp(1438870917.0),
+            'url': (
+                'http://nqigfqrxnkwcqmiq.onion/'
+                'wiki/index.php#Whistleblowing'
+            )
+        }
+    ]
